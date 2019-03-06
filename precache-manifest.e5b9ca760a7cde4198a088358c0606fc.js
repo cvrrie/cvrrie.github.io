92b5aa0c57d02588b8732de0b230c01e60ba4028
Carrie <carrierong37@gmail.com>
2019-03-05 23:01:13 -0500

    Add illustrations

---
+++ precache-manifest.e5b9ca760a7cde4198a088358c0606fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b22a36f122e19464a190",
+    "url": "/static/css/main.3798b16d.chunk.css"
+  },
+  {
+    "revision": "b22a36f122e19464a190",
+    "url": "/static/js/main.b22a36f1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3534d905048e8c03ca7f",
+    "url": "/static/js/2.3534d905.chunk.js"
+  },
+  {
+    "revision": "90b7234e8b35fd9281dc01fa1f956891",
+    "url": "/static/media/sunny.90b7234e.svg"
+  },
+  {
+    "revision": "9a13ab3a8e9e378c7c47a86de63dd2ab",
+    "url": "/static/media/planet.9a13ab3a.svg"
+  },
+  {
+    "revision": "12020fd07065ad23acd63cfa5b6630fb",
+    "url": "/static/media/banana.12020fd0.svg"
+  },
+  {
+    "revision": "fa09f929e9483b96009841cdeea291a1",
+    "url": "/static/media/dragonfruit.fa09f929.svg"
+  },
+  {
+    "revision": "6f4e8b56ebdc5d4e13b8a811e9396d3b",
+    "url": "/index.html"
+  }
+];
